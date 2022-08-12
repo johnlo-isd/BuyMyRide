13ea2a80ff6ea2e1a70621f61890ba5aa97e0771
John Loeppky <loeppkyj@gmail.com>
2022-08-12 16:18:15 -0500

    Initial Commit

---
+++ BuyMyRide/BuyMyRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyMyRideApp.swift
+//  BuyMyRide
+//
+//  Created by John Loeppky on 2022-08-12.
+//
+
+import SwiftUI
+
+@main
+struct BuyMyRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
